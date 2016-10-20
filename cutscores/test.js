dfabefbe8653e5523b699cd8e7be4a82f548e92e
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-20 10:29:22 -0400

    Move cutscores data into cutscores dir

---
+++ cutscores/test.js
@@ -0,0 +1,36 @@
+const { readdirSync: dir, readFile: file } = require('fs');
+const { join } = require('path');
+const validate = require('jsonschema').validate;
+const schema = require('./schema.json');
+
+// get all the data files
+const dataFiles = []
+  .concat(
+    dir(join(__dirname, './base')).map(f => join(__dirname, './base', f)),
+    dir(join(__dirname, './sgp')).map(f => join(__dirname, './sgp', f))
+  )
+  .filter(filename => filename.substr(-4) === 'json');
+
+let results = [];
+
+// check each file against the schema
+dataFiles.forEach(filename => {
+  file(filename, 'utf8', (err, contents) => {
+    const result = validate(JSON.parse(contents), schema);
+
+    // bail out completely if an error is found
+    if (result.errors.length) {
+      console.error(result);
+      console.log('');
+      console.error(`There is a problem with ${filename}`);
+      process.exit(1);
+    } else {
+      results.push(`${filename} is valid`);
+    }
+
+    // hooray, no errors!
+    if (results.length === dataFiles.length) {
+      console.log('All data files are valid!');
+    }
+  })
+});
