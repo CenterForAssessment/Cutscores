f18b1698e7a69d785ad92b5dd3f044ef1b2f2a68
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-11-29 15:45:32 -0500

    Add schema for student data and test before commits

---
+++ students/test.js
@@ -0,0 +1,36 @@
+const dir = require('fs').readdirSync;
+const file = require('fs').readFile;
+const join = require('path').join;
+const validate = require('jsonschema').validate;
+const schema = require('./schema.json');
+
+// get all the data files
+const dataFiles = []
+  .concat(
+    dir(join(__dirname)).filter(f => f !== 'schema.json').map(f => join(__dirname, f))
+  )
+  .filter(filename => filename.substr(-4) === 'json');
+
+var results = [];
+
+// check each file against the schema
+dataFiles.forEach(filename => {
+  file(filename, 'utf8', (err, contents) => {
+    const result = validate(JSON.parse(contents), schema);
+
+    // bail out completely if an error is found
+    if (result.errors.length) {
+      console.error(result);
+      console.log('');
+      console.error(`There is a problem with ${filename}`);
+      process.exit(1);
+    } else {
+      results.push(`${filename} is valid`);
+    }
+
+    // hooray, no errors!
+    if (results.length === dataFiles.length) {
+      console.log('All data files are valid!');
+    }
+  })
+});
